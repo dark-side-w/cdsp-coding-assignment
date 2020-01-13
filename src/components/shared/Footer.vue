2bbabd67d32564a55b89f1f4c3a43d3c0b1d3db9
Dariusz Wytrykus <dariusz.wytrykus@sagiton.pl>
2020-01-13 22:46:40 +0100

    WIP: Footer

--- src/components/shared/Footer.vue
+++ src/components/shared/Footer.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    <h1>This is the footer</h1>
+  <div class="footer">
+    <div class="nav">
+      <a href="#">Lorem</a>
+      <a href="#">Lorem</a>
+      <a href="#">Lorem Ipsum</a>
+    </div>
+    <hr>
+    <div class="nav">
+      <a href="#">Lorem ipsum</a>
+    </div>
   </div>
 </template>
 
@@ -10,6 +18,25 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.footer {
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-color: black;
+  padding: 30px;
+  .nav {
+    a {
+      font-sizet: 14px;
+      margin-right: 30px;
+      color: white;
+    }
+  }
+  hr {
+    background-color: white;
+    margin: 30px 0;
+    color: white;
+  }
+}
 </style>
